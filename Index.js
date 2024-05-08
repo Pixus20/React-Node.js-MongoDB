249adb82a3addbe74adf11fed8bb1167d5dd0b0b
Pixus20 <mihalcukvasa01@gmail.com>
2024-05-08 13:46:45 +0300

    start creating server for blog

---
+++ Index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+
+
+const app =express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+   res.send('111 Hello world!')
+});
+
+app.post('/auth/login', (req,res)=>{
+   console.log(req.body);
+   res.json({
+      success:true,
+      
+   });
+})
+
+
+app.listen(4444, (err)=>{
+   if (err){
+      return console.log(err);
+   }
+   console.log("Server OK!");
+});
